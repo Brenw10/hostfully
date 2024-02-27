8c5b3ff7bd896e0c9943c715a69d94519f0977b0
Brendon Mota <bw.bwctm@gmail.com>
2024-02-27 00:03:59 -0300

    format

--- src/booking/BookingTable/constants.ts
+++ src/booking/BookingTable/constants.ts
@@ -11,14 +11,14 @@ export const COLUMNS = [
     title: 'Start Date',
     key: 'startDate',
     dataIndex: 'dates',
-    render: (dates: TBookingDates) => dates[0].format('MM/DD/YYYY'),
+    render: (dates: TBookingDates) => dates[0].format('YYYY-MM-DD'),
     sorter: (a: TBooking, b: TBooking) => a.dates[0].diff(b.dates[0]),
   },
   {
     title: 'End Date',
     key: 'endDate',
     dataIndex: 'dates',
-    render: (dates: TBookingDates) => dates[1].format('MM/DD/YYYY'),
+    render: (dates: TBookingDates) => dates[1].format('YYYY-MM-DD'),
     sorter: (a: TBooking, b: TBooking) => a.dates[1].diff(b.dates[1]),
   },
 ];
